6aa6c98f5728890ea763e3fa38f93937e1b7a96a
Thanhqh <114939921+Thanhqh@users.noreply.github.com>
2022-10-03 23:20:42 +0700

    upload file

---
+++ js/moi.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+var typed = new Typed('.element', {
+               	strings: ["Nguyễn Công Thành"],
+       	 	typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
